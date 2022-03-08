f5b6868cac1afb83d3305ecb156f6a13bcb37e03
Dants0 <gui.dg1@hotmail.com>
2022-03-08 16:52:38 -0300

    more index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
-const express = require("expres");
+const cookieSession = require("cookie-session");
+const express = require("express");
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
 const app = express();
 
-console.log("hello");
+app.use(
+  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+);
 
-app.listen("3000", ()=>{
-    console.log("Server is running in 3000 Port");
-})
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Server is running!");
+});
